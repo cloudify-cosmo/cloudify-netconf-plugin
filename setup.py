3883e0df283139341189c7f95991c18cfe64f9a6
EarthmanT <trammell@cloudify.co>
2022-06-12 20:57:18 -0400

    fake release 3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-netconf-plugin',
-    version='0.4.8',
+    version='0.4.9',
     description='Cloudify Netconf plugin',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
