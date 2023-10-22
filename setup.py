b819455e58d07337485e2b8888efbdeaad599d1f
EarthmanT <EarthmanT@users.noreply.github.com>
2023-10-22 13:50:01 -0400

    bump version

--- setup.py
+++ setup.py
@@ -11,11 +11,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+import re
+import pathlib
+
 from setuptools import setup
+
+
+def get_version():
+    current_dir = pathlib.Path(__file__).parent.resolve()
+    with open(os.path.join(current_dir, 'cloudify_netconf/__version__.py'),
+              'r') as outfile:
+        var = outfile.read()
+        return re.search(r'\d+.\d+.\d+', var).group()
+
 
 setup(
     name='cloudify-netconf-plugin',
-    version='0.4.11',
+    version='0.4.12',
     description='Cloudify Netconf plugin',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
